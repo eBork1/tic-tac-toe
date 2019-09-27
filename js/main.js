9fddcb2084488bf36f436e463ebaea0bf1d21a6a
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-27 16:34:08 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 document.body.onload = createGrid;
 var mainDiv = document.getElementById("mainDiv");
 
-clicks = 0
+let ar = new Array(9).fill(0);
+console.log(ar);
 
-function sign() {               // mark the board with X or O
-    clicks++;
-    if (clicks % 2 == 0) {
+clicks = 0
+score = 0
+
+// mark the board with X or O
+function sign() {
+    console.log(this);
+
+    clicks++;                 // adds clicks. even numbers are X, odd numbers are O
+    if (clicks % 2) {
         this.innerHTML = "x";
+        TurnText.innerHTML = "O's turn!";
+        ;
+
     } else {
         this.innerHTML = "o";
+        TurnText.innerHTML = "X's turn!";
     }
 }
 
+//check scoring value
+
+// check for win
+var wins = [];
+
+//reset button
+
+//set board
 function createGrid() {
 
     //title 
@@ -33,15 +52,18 @@ function createGrid() {
         for (var j = 0; j < 3; j++) {
             //create the 9 columns 
             var col = document.createElement('div');
-            col.setAttribute('class', 'col-3 border');
+            col.setAttribute('class', 'col-3 border text-center');
+            col.id = "row" + i + "col" + j;
             col.addEventListener('click', sign);
 
             row.appendChild(col);
-
         }
         container.appendChild(row);
     }
     mainDiv.appendChild(container);
-
-
 }
+
+//display turn
+var TurnText = document.createElement('h4');
+TurnText.innerHTML = "It's X's turn!";
+mainDiv.appendChild(TurnText);
