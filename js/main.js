cfbc56b22c26cdd1e75e9cf5db8253125d481087
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-28 22:32:25 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,63 @@
 document.body.onload = createGrid;
 var mainDiv = document.getElementById("mainDiv");
+var status;
+var reset; 
 
 let ar = new Array(9).fill(0);
 console.log(ar);
-
+var wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 clicks = 0
-score = 0
 
 // mark the board with X or O
 function sign() {
     console.log(this);
+    var q = this.id.split("col");
+    var idx = (Number(q[0]) * 3 + Number(q[1]));
+    if (ar[idx] === 0) {
+        clicks++;                 // adds clicks. even numbers are X, odd numbers are O
+        if (clicks % 2) {
+            this.innerHTML = "x";
+            TurnText.innerHTML = "O's turn!";
+            ar[idx] = 2;
 
-    clicks++;                 // adds clicks. even numbers are X, odd numbers are O
-    if (clicks % 2) {
-        this.innerHTML = "x";
-        TurnText.innerHTML = "O's turn!";
-        ;
-
-    } else {
-        this.innerHTML = "o";
-        TurnText.innerHTML = "X's turn!";
+        } else {
+            this.innerHTML = "o";
+            TurnText.innerHTML = "X's turn!";
+            ar[idx] = 1;
+        }
+        checkWin();
     }
 }
 
-//check scoring value
+//check value
+function checkValue(a, b, c) {
+    if (ar[a] == 0 || ar[b] == 0 || ar[c] == 0) {
+        return 0;
+    }
+    var sum = ar[a] + ar[b] + ar[c];
+    if (sum == 3) {
+        return 1;
+    }
+    if (sum == 6) {
+        return 2;
+    }
+    return 0;
+}
 
 // check for win
-var wins = [];
+function checkWin() {
+    for (let i = 0; i < wins.length; i++) {
+        var a = checkValue(wins[i][0], wins[i][1], wins[i][2]);
+        if (a) {
+            status = "win";
+            var TurnText = document.createElement('h4');
+            TurnText.innerHTML = "X WINS!";
+            mainDiv.appendChild(TurnText);
+            ar.fill(3);
+            break;
+        }
+    }
+}
 
 //reset button
 
@@ -53,7 +84,7 @@ function createGrid() {
             //create the 9 columns 
             var col = document.createElement('div');
             col.setAttribute('class', 'col-3 border text-center');
-            col.id = "row" + i + "col" + j;
+            col.id = i + "col" + j;
             col.addEventListener('click', sign);
 
             row.appendChild(col);
