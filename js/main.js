74a32f3c93bdd86b6b0f08eb31ce44851f5f47fe
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-30 09:51:43 -0400

    clean up code

--- js/main.js
+++ js/main.js
@@ -1,65 +1,10 @@
 document.body.onload = createGrid;
 var mainDiv = document.getElementById("mainDiv");
-var status;
- 
 
 let ar = new Array(9).fill(0);
 console.log(ar);
 var wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 clicks = 0
-
-// mark the board with X or O
-function sign() {
-    console.log(this);
-    var q = this.id.split("col");
-    var idx = (Number(q[0]) * 3 + Number(q[1]));
-    if (ar[idx] === 0) {
-        clicks++;                 // adds clicks. even numbers are X, odd numbers are O
-        if (clicks % 2) {
-            this.innerHTML = "x";
-            TurnText.innerHTML = "O's turn!";
-            ar[idx] = 2;
-
-        } else {
-            this.innerHTML = "o";
-            TurnText.innerHTML = "X's turn!";
-            ar[idx] = 1;
-        }
-        checkWin();
-    }
-}
-
-//check value
-function checkValue(a, b, c) {
-    if (ar[a] == 0 || ar[b] == 0 || ar[c] == 0) {
-        return 0;
-    }
-    var sum = ar[a] + ar[b] + ar[c];
-    if (sum == 3) {
-        return 2;
-    }
-    if (sum == 6) {
-        return 1;
-    }
-    return 0;
-}
-
-// check for win
-function checkWin() {
-    for (let i = 0; i < wins.length; i++) {
-        var a = checkValue(wins[i][0], wins[i][1], wins[i][2]);
-        if (a) {
-            var TurnText = document.createElement('h4');
-            TurnText.innerHTML = "Player " + a + " WINS!";
-            mainDiv.appendChild(TurnText);
-            ar.fill(3);
-            console.log(ar);
-            break;
-        }
-    }
-}
-
-//reset button
 
 //set board
 function createGrid() {
@@ -94,7 +39,60 @@ function createGrid() {
     mainDiv.appendChild(container);
 }
 
+// mark the board with X or O
+function sign() {
+    console.log(this);
+    var q = this.id.split("col");
+    var idx = (Number(q[0]) * 3 + Number(q[1]));
+    if (ar[idx] === 0) {
+        clicks++;                 // adds clicks. even numbers are X, odd numbers are O
+        if (clicks % 2) {
+            this.innerHTML = "x";
+            TurnText.innerHTML = "O's turn!";
+            ar[idx] = 2;
+
+        } else {
+            this.innerHTML = "o";
+            TurnText.innerHTML = "X's turn!";
+            ar[idx] = 1;
+        }
+        checkWin();
+    }
+}
+
 //display turn
 var TurnText = document.createElement('h4');
 TurnText.innerHTML = "It's X's turn!";
 mainDiv.appendChild(TurnText);
+
+// check for win
+function checkWin() {
+    for (let i = 0; i < wins.length; i++) {
+        var a = checkValue(wins[i][0], wins[i][1], wins[i][2]);
+        if (a) {
+            var TurnText = document.createElement('h4');
+            TurnText.innerHTML = "Player " + a + " WINS!";
+            mainDiv.appendChild(TurnText);
+            ar.fill(3);
+            console.log(ar);
+            break;
+        }
+    }
+}
+
+//check value
+function checkValue(a, b, c) {
+    if (ar[a] == 0 || ar[b] == 0 || ar[c] == 0) {
+        return 0;
+    }
+    var sum = ar[a] + ar[b] + ar[c];
+    if (sum == 3) {
+        return 2;
+    }
+    if (sum == 6) {
+        return 1;
+    }
+    return 0;
+}
+
+//reset button
